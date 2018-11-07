2a6ceb79b1c1ca0d834a2e18420d7ed27ceedafb
Olessia Potapova <olessia.potapova@gmail.com>
2018-11-07 06:31:27 -0300

    added paidUser class using factory functions approach

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-class UserCreator {
-    constructor  (name, score) {
-        this.name = name
-        this.score = score
-    }
-    increment() {
-        this.score++;
-    }
-    login() {
-        this.name = console.log(this.name);
+function userCreator(name, score) {
+    const newUser = Object.create(userFunctions);
+    newUser.name = name;
+    newUser.score = score;
+    return newUser;
+}
+
+userFunctions = {
+    increaseScore: function() {
+        this.score++
+    },
+    greet: function(){
+        console.log('hi ' + this.name);
     }
 }
 
-const user1 = new UserCreator("phil", 2);
+function paidUser(paidName, paidScore, accountBalance){
+    const newPaidUser = userCreator(paidName, paidScore); //right now proto is pointing at userFuntions
+    Object.setPrototypeOf(newPaidUser, paidUserFunctions); // changes proto to paidUserFunction
+    newPaidUser.accountBalance = accountBalance; // not sure about this one 
+    return newPaidUser;
+}
 
-console.log(user1);
+paidUserFunctions = {
+    changeAccountBalance: function() {
+        this.accountBalance *= 2;
+    }
+}
 
-user1.increment();
+Object.setPrototypeOf(paidUserFunctions, userFunctions) // sets paidUserFunction's prototype chain to look up to userFunctions
 
-console.log(user1);
 
-user1.login();
+var user1 = userCreator("oli", 2);
+
+var user2 = paidUser("seba", 5, 100);
+console.log(user2);
+
+user2.changeAccountBalance();
+user2.greet();
+console.log(user2);
+
+
+//user1.greet;
+//subclassing with factory functions
+//add paidUser class that has the same properites and functions of UserCreator 
+//with its own parameters of 'paidName', 'paidScore', 'accountBalance' and increaseBalance method
