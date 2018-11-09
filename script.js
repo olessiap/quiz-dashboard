95a5b2e572fe4d8f1a992f784c03313153b3d3e4
Olessia Potapova <olessia.potapova@gmail.com>
2018-11-09 17:17:23 -0300

    added input fields to track with cookies

--- script.js
+++ script.js
@@ -25,16 +25,28 @@ class paidUserCreator extends userCreator {
 
 
 const user1 = new userCreator("Oli", 2);
-
-console.log(user1);
-user1.login();
-//
-
 const user2 = new paidUserCreator("Seba", 1, 100);
-console.log(user2);
 
+user1.login();
 user2.increaseScore();
-console.log(user2);
-
 user2.increaseAccountBalance();
-console.log(user2);
+
+//document.cookie = 'name=oli; expires=Fri, 31 Dec 2024 23:59:59 GMT';
+// console.log(document.cookie);
+// console.log("suuuuup");
+
+
+var myCookies = {};
+var user = document.getElementById("user");
+var age = document.getElementById("age");
+var output = document.getElementById('output');
+
+function saveCookie() {
+    myCookies["_user"] = user.value;
+    myCookies["_uage"] = age.value;
+    document.cookie = ""; 
+    
+    //set up expiary 
+    var expiration = new Date(Date.now() +60 * 1000).toString();
+    console.log(expiration);
+}
